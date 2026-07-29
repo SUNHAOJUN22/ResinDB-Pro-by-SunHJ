c8865b6c63c81d35e4fe810e20f49156970ae525
Clark_Tsao <resj666@gmail.com>
2026-07-29 15:26:55 +0800

    feat: expose seeded rng primitives

--- src/compute/index.ts
+++ src/compute/index.ts
@@ -5,6 +5,7 @@ export * from './computeEvidence';
 export * from './kernelRegistry';
 export * from './leastSquares';
 export * from './random';
+export * from './seededRng';
 export * from './taskProtocol';
 export * from './transferables';
 export * from './types';
