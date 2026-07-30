a67f74a446cb55c7c7b517939db519b214ed77c7
Clark_Tsao <resj666@gmail.com>
2026-07-30 12:27:25 +0800

    feat: export kmeans browser calibration modules

--- src/compute/index.ts
+++ src/compute/index.ts
@@ -7,6 +7,8 @@ export * from './gaussianProcess';
 export * from './kernelRegistry';
 export * from './kmeansAssignment';
 export * from './kmeansBackendPolicy';
+export * from './kmeansBackendProfileStore';
+export * from './kmeansBrowserBenchmark';
 export * from './leastSquares';
 export * from './linearAlgebra';
 export * from './nonlinearLeastSquares';
